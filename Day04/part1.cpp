38129bbfb30320e2c96f2d10cb9050db8da4bdee
Simone Laierno <simonelaierno@gmail.com>
2020-12-05 18:10:48 +0100

    Makefile not ignored now, day04 functional twist

--- Day04/part1.cpp
+++ Day04/part1.cpp
@@ -7,23 +7,16 @@
 #include <numeric>
 #include <map>
 #include <execution>
+#include <range/v3/all.hpp>
 
+namespace views = ranges::views;
 auto split(std::string to_split, const std::vector<std::string>& delimiters) {
     std::vector<std::string> ret;
     for(size_t pos = 0; pos != std::string::npos;) {
-        std::vector<size_t> findings(delimiters.size());
-        std::transform(
-            std::execution::par_unseq,
-            std::begin(delimiters), std::end(delimiters), 
-            std::begin(findings), 
-            [&to_split](auto d){ return to_split.find(d);});
-        auto it = std::min_element(
-            std::execution::par_unseq, 
-            std::begin(findings), std::end(findings));
-        pos = *it;
-        auto delimiter = delimiters.at(std::distance(std::begin(findings), it));
+        const auto it = ranges::min_element(delimiters, [&to_split](auto a, auto b){ return to_split.find(a) < to_split.find(b);});
+        pos = to_split.find(*it);
         ret.push_back(to_split.substr(0,pos));
-        if(pos != std::string::npos) to_split.erase(0, pos + delimiter.size());
+        if(pos != std::string::npos) to_split.erase(0, pos + it->size());
     }
     return ret;
 }
@@ -42,21 +35,22 @@ int main() {
         std::begin(input), std::end(input), 
         0, std::plus<>(),
         [&entries](const auto& passport) {
-            const auto passport_map = [&]() {
-                const auto passport_split = split(passport, {"\n", " "});
-                std::map<std::string, std::string> passport_map;
-                std::ranges::transform(
-                    std::begin(passport_split), std::end(passport_split), 
-                    std::inserter(passport_map, std::end(passport_map)), 
-                    [](auto s) {
-                        auto s_split = split(s, {":"});
+            const auto passport_map = 
+                ranges::to<std::map<std::string, std::string>>(
+                    views::all(passport)
+                  | views::replace('\n', ' ')
+                  | views::split(' ')
+                  | views::transform(ranges::to<std::string>())
+                  | views::transform([](auto s) {
+                        const auto s_split = ranges::to<std::vector<std::string>>(
+                          s 
+                        | views::split(':')
+                        | views::transform(ranges::to<std::string>())); 
                         return std::pair(s_split[0], s_split[1]);
-                    });
-                return passport_map;
-            }();
-            return std::all_of(
-                std::begin(entries), 
-                std::end(entries), 
+                    })
+                );
+            return ranges::all_of(
+                entries,
                 [&passport_map](const auto& entry) {return passport_map.find(entry) != passport_map.end();});
     });
     std::cout << valid_passport_cnt << std::endl;
--- Day04/part2.cpp
+++ Day04/part2.cpp
@@ -9,23 +9,16 @@
 #include <set>
 #include <functional>
 #include <execution>
+#include <range/v3/all.hpp>
 
+namespace views = ranges::views;
 auto split(std::string to_split, const std::vector<std::string>& delimiters) {
     std::vector<std::string> ret;
     for(size_t pos = 0; pos != std::string::npos;) {
-        std::vector<size_t> findings(delimiters.size());
-        std::transform(
-            std::execution::par_unseq,
-            std::begin(delimiters), std::end(delimiters), 
-            std::begin(findings), 
-            [&to_split](auto d){ return to_split.find(d);});
-        auto it = std::min_element(
-            std::execution::par_unseq, 
-            std::begin(findings), std::end(findings));
-        pos = *it;
-        auto delimiter = delimiters.at(std::distance(std::begin(findings), it));
+        const auto it = ranges::min_element(delimiters, [&to_split](auto a, auto b){ return to_split.find(a) < to_split.find(b);});
+        pos = to_split.find(*it);
         ret.push_back(to_split.substr(0,pos));
-        if(pos != std::string::npos) to_split.erase(0, pos + delimiter.size());
+        if(pos != std::string::npos) to_split.erase(0, pos + it->size());
     }
     return ret;
 }
@@ -37,15 +30,9 @@ constexpr bool in_bound(const T val, const T min, const T max) {
 
 int main() {
     const std::map<std::string, std::function<bool(std::string)>> entries {
-        {"byr", [](std::string s) {
-            return in_bound(std::stoi(s), 1920, 2002);
-        }},
-        {"iyr", [](std::string s) {
-            return in_bound(std::stoi(s), 2010, 2020);
-        }},
-        {"eyr", [](std::string s) {
-            return in_bound(std::stoi(s), 2020, 2030);
-        }},
+        {"byr", [](std::string s) { return in_bound(std::stoi(s), 1920, 2002); }},
+        {"iyr", [](std::string s) { return in_bound(std::stoi(s), 2010, 2020); }},
+        {"eyr", [](std::string s) { return in_bound(std::stoi(s), 2020, 2030); }},
         {"hgt", [](std::string s) {
             if(const auto mu_pos = s.find_first_not_of("0123456789"); mu_pos != std::string::npos) {
                 if(auto match = s.substr(mu_pos); "cm" == match) {
@@ -63,9 +50,7 @@ int main() {
             const std::set<std::string> colors {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
             return colors.find(s) != colors.end();
         }},
-        {"pid", [](std::string s) {
-            return (s.length() == 9) && (std::all_of(std::begin(s), std::end(s), ::isdigit));
-        }},
+        {"pid", [](std::string s) { return (s.length() == 9) && (std::all_of(std::begin(s), std::end(s), ::isdigit)); }},
         /*"cid"*/
     };
     const auto input = []() -> std::vector<std::string> {
@@ -80,25 +65,24 @@ int main() {
         std::begin(input), std::end(input), 
         0, std::plus<>(),
         [&entries](const auto& passport) {
-            const auto passport_map = [&]() {
-                const auto passport_split = split(passport, {"\n", " "});
-                std::map<std::string, std::string> passport_map;
-                std::transform(
-                    std::begin(passport_split), std::end(passport_split), 
-                    std::inserter(passport_map, std::end(passport_map)), 
-                    [](auto s) {
-                        auto s_split = split(s, {":"});
+            const auto passport_map = 
+                ranges::to<std::map<std::string, std::string>>(
+                    views::all(passport)
+                  | views::replace('\n', ' ')
+                  | views::split(' ')
+                  | views::transform(ranges::to<std::string>())
+                  | views::transform([](auto s) {
+                        const auto s_split = ranges::to<std::vector<std::string>>(
+                          s 
+                        | views::split(':')
+                        | views::transform(ranges::to<std::string>())); 
                         return std::pair(s_split[0], s_split[1]);
-                    });
-                return passport_map;
-            }();
-            return std::all_of(
-                std::execution::par_unseq, 
-                std::begin(entries), 
-                std::end(entries), 
+                    })
+                );
+            return ranges::all_of(
+                entries,
                 [&passport_map](const auto& entry) {
-                    auto found = passport_map.find(entry.first);
-                    if(found != passport_map.end()) {
+                    if(auto found = passport_map.find(entry.first); found != passport_map.end()) {
                         return entry.second(found->second);
                     } else return false;
                 });
