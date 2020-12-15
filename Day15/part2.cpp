dd3cfc357ac9c53ba68f79d8fb067b2a85833865
Simone Laierno <simonelaierno@gmail.com>
2020-12-15 11:25:06 +0100

    Day15 part2.cpp way faster with a vector

--- Day15/part2.cpp
+++ Day15/part2.cpp
@@ -5,16 +5,16 @@
 #include <utils.hpp>
 
 int main() {
-    const auto input = AoC::get_input("input.txt", ',', [](const std::string& s) { return std::stoi(s); });
-    std::map<int, size_t> last_insertion;
-    size_t idx = 0;
-    int last_say = input[0];
-    const auto map_insert = [&last_insertion, &last_say, &idx](const auto val) {
+    const auto input = AoC::get_input("input.txt", ',', [](const std::string& s) { return std::stoul(s); });
+    std::vector<unsigned> last_insertion(30000000);
+    unsigned idx = 1;
+    unsigned last_say = input[0];
+    const auto map_insert = [&last_insertion, &last_say, &idx](const unsigned val) {
         last_insertion[last_say] = idx++;
         last_say = val;
     };
-    while(idx < input.size() - 1) map_insert(input[idx+1]);
-    while(idx < 30000000 - 1) map_insert(last_insertion.find(last_say) != last_insertion.end() ? idx - last_insertion[last_say] : 0);
+    while(idx < input.size()) map_insert(input[idx]);
+    while(idx < 30000000) map_insert(last_insertion[last_say] ? idx - last_insertion[last_say] : 0);
     std::cout << last_say << std::endl;
     return 0;
 }
